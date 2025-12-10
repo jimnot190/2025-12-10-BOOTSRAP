4280dfe7f29e6d3e450f958e1e8ba73a0d0f9c3c
jimnot190 <sxop07@gmail.com>
2025-12-10 10:54:17 +0800

    Add files via upload

---
+++ altertest.js
@@ -0,0 +1,24 @@
+    document.addEventListener("DOMContentLoaded", function () {
+
+            const alertPlaceholder = document.getElementById('liveAlertPlaceholder');
+
+            const appendAlert = (message, type) => {
+                const wrapper = document.createElement('div');
+    wrapper.innerHTML = `
+    <div class="alert alert-${type} alert-dismissible" role="alert">
+        <div>${message}</div>
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+    `;
+    alertPlaceholder.append(wrapper);
+            };
+
+    const alertTrigger = document.getElementById('liveAlertBtn');
+
+    if (alertTrigger) {
+        alertTrigger.addEventListener('click', () => {
+            appendAlert('ZZZZZZZZ!', 'success');
+        });
+            }
+
+        });
